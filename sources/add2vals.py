4b6adfadb67b1b6d2f014c256c6fe2cd2249d841
pragadeesh57 <belivertamil57@gmail.com>
2026-09-17 11:31:51 +0530

    Create add2vals.py

---
+++ sources/add2vals.py
@@ -0,0 +1,16 @@
+from calc import add
+
+
+def main():
+    num1 = 10
+    num2 = 20
+
+    result = add(num1, num2)
+
+    print("Number 1:", num1)
+    print("Number 2:", num2)
+    print("Addition Result:", result)
+
+
+if __name__ == "__main__":
+    main()
